24034ad00b4a8477a84af0a610b5aca2cab5aab0
liuyue <18519127046@163.com>
2020-12-11 11:13:00 +0800

    Release 0.0.7

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import typescript from '@rollup/plugin-typescript'
 import {babel} from '@rollup/plugin-babel'
 import json from '@rollup/plugin-json'
-// import pkg from './package.json'
 import babelConfig from './babel.config'
 import copy from 'rollup-plugin-copy'
 
