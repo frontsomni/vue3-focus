f29e1290a6954207247d2c50a3e0b9d620f18d8c
liuyue <18519127046@163.com>
2020-12-17 17:35:21 +0800

    Release: 0.0.29

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,23 @@
-module.exports = {}
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV !== 'production' ? 0 : 2,
+    'no-useless-escape': 0,
+    'no-empty': 0,
+    "import/prefer-default-export": "off"
+  },
+};
